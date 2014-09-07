2b092d89cb9fc61d6bf297f7f23ee7f2590994d4
isana <g0821009@gmail.com>
2014-09-08 08:51:44 +0900

    first version

---
+++ serial-port/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(12, 24)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(260, 112)
+        Me.ListBox1.TabIndex = 0
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ポートリスト"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 171)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "タブ後のウェイト(ミリ秒)"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 12
+        Me.ListBox2.Location = New System.Drawing.Point(12, 186)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(260, 64)
+        Me.ListBox2.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(234, 8)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 12)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(234, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 12)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Label4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "タブ後ウェイトv1.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ serial-port/Form1.vb
@@ -0,0 +1,161 @@
+﻿Imports System
+Imports System.IO.Ports
+
+Public Class Form1
+    Dim weit As String
+    Dim appPath As String = System.IO.Directory.GetCurrentDirectory() & "\"
+    Dim fileName As String = ".serial-port.ini"
+    Dim setting_wf As IO.StreamWriter
+    Dim Encode As System.Text.Encoding = System.Text.Encoding.GetEncoding("Shift-JIS")
+
+    Private Sub createListBox2()
+        ListBox2.Items.Add(250)
+        ListBox2.Items.Add(500)
+        ListBox2.Items.Add(1000)
+        ListBox2.Items.Add(1500)
+    End Sub
+
+    Private Function importSetting()
+        Dim settingFile As IO.StreamReader
+        Dim sa As New ArrayList
+        Dim stBuffer As String
+
+        Debug.WriteLine("import Setting start.")
+        Try
+            settingFile = New IO.StreamReader(appPath + fileName, Encode, False)
+            While (settingFile.Peek() >= 0)
+                stBuffer = settingFile.ReadLine()
+                Debug.WriteLine(stBuffer)
+                sa.Add(stBuffer)
+            End While
+            settingFile.Close()
+        Catch ex As Exception
+            Debug.WriteLine("file open error:")
+            Debug.WriteLine(ex)
+        End Try
+        Return sa
+    End Function
+
+    Private Sub exportSetting(ByVal settingLines As String)
+        Dim settingFile As IO.StreamWriter
+        Try
+            '既に存在するテキストに追加する場合は第２引数をTrueにする。
+            settingFile = New IO.StreamWriter(appPath & fileName, False, Encode)
+            settingFile.Write(settingLines)
+            settingFile.Close()
+        Catch ex As Exception
+            Debug.WriteLine("Error:")
+            Debug.WriteLine(ex)
+        End Try
+        Debug.WriteLine("save Settings")
+        Debug.WriteLine(appPath & fileName)
+        Debug.WriteLine(settingLines)
+    End Sub
+
+    '文字コードにShiftJISを指定。(UTF8の場合は指定不要)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim portnames As String()
+        Dim portname As String
+        Dim settingArray As New ArrayList
+
+        createListBox2()
+
+        portnames = SerialPort.GetPortNames()
+        Debug.WriteLine(portnames)
+
+        If IsArray(portnames) Then
+            Debug.WriteLine("port name is Nothing")
+        Else
+            For Each portname In portnames
+                ListBox1.Items.Add(portname)
+            Next portname
+        End If
+
+        settingArray = importSetting()
+
+        If settingArray.Count > 0 Then
+            Dim setting_item() As String
+            For Each setting_line As String In settingArray
+
+                ' TODO delimiter error
+                setting_item = setting_line.Split(":"c)
+                Select Case setting_item(0)
+                    Case "portname"
+                        Label3.Text = setting_item(1)
+                    Case "weit"
+                        Label4.Text = setting_item(1)
+                End Select
+            Next
+        Else
+            Label3.Text = ""
+            Label4.Text = "500"
+        End If
+
+        Try
+            If (ListBox1.FindStringExact(Label3.Text) = ListBox.NoMatches) Then
+                Debug.WriteLine("port name no matches in port list.")
+                If Label3.Text = "" Then
+                    SerialPort1.PortName = "COM1"
+                    SerialPort1.Open()
+                Else
+                    SerialPort1.PortName = Label3.Text
+                    SerialPort1.Open()
+                End If
+            Else
+                SerialPort1.PortName = Label3.Text
+                SerialPort1.Open()
+            End If
+        Catch ex As Exception
+            Debug.WriteLine("error:")
+            Debug.WriteLine(ex)
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Closed(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Closed
+        Dim settingLines = String.Empty
+        settingLines &= "portname:" & SerialPort1.PortName & System.Environment.NewLine
+        settingLines &= "weit:" & ListBox2.Text
+        exportSetting(settingLines)
+        Debug.WriteLine("finished")
+        If SerialPort1.IsOpen = True Then   'ポートオープン済み
+            SerialPort1.Close()              'ポートクローズ
+        End If
+    End Sub
+
+    Private Sub ListBox1_Click(sender As Object, e As EventArgs) Handles ListBox1.Click
+        If ListBox1.Items.Count = 0 Then
+
+        Else
+            If SerialPort1.IsOpen = True Then   'ポートオープン済み
+                SerialPort1.Close()              'ポートクローズ
+            End If
+            SerialPort1.PortName = ListBox1.Text
+            Label3.Text = SerialPort1.PortName
+            SerialPort1.Open()
+            Debug.WriteLine("open " + SerialPort1.PortName)
+        End If
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim received_strings As String
+        Dim c As Char
+
+        received_strings = SerialPort1.ReadLine
+        For Each c In received_strings
+            Debug.Write(c)
+            SendKeys.SendWait(c)
+            If c = vbTab Then
+                System.Threading.Thread.Sleep(weit)
+            End If
+        Next
+    End Sub
+
+    Private Sub ListBox2_Click(sender As Object, e As EventArgs) Handles ListBox2.Click
+        If ListBox2.Text <> "" Then
+            weit = ListBox2.Text
+            Label4.Text = weit
+            Debug.WriteLine("weit is " + weit)
+        End If
+    End Sub
+End Class
